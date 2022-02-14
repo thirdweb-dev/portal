938bacef1363b13cf5d5c9aecb3471a12a192aa6
razacodespython <76620622+razacodespython@users.noreply.github.com>
2022-02-14 22:20:17 +0100

    bundles descr (#9)
    * bundles descr
    
    * Update portal/learn/modules,bundle.mdx
    
    Co-authored-by: Nacho Iacovino <50103937+nachoiacovino@users.noreply.github.com>
    
    * nav change
    
    * nav change
    
    * nft drop title change
    
    * Fix deployment
    
    * url change
    
    * added erc
    
    * nav
    
    side nav
    before and after
    
    Co-authored-by: Nacho Iacovino <50103937+nachoiacovino@users.noreply.github.com>
    Co-authored-by: Nacho Iacovino <nacho.iacovino@gmail.com>

--- components/layout/portal-shell/Sidebar/index.tsx
+++ components/layout/portal-shell/Sidebar/index.tsx
@@ -39,6 +39,11 @@ export const PortalSidebar: React.FC = () => {
               href="/learn/modules/nft-collection"
             />
             <SidebarItem
+              label="Bundle Collection"
+              matchExact
+              href="/learn/modules/bundle-collection"
+            />
+            <SidebarItem
               label="NFT Drop"
               matchExact
               href="/learn/modules/nft-drop"
